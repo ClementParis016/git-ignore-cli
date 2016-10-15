b1aa3250756ff9a0b1a0677d53cfc03e2ec7e8e1
ClementParis016 <clementparis016@gmail.com>
2016-10-15 11:40:06 +0200

    Boilerplate for testing

---
+++ test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const expect = require('chai').expect;
+const gitIgnore = require('./lib/index.js');
+
+describe('.gitignore path lookup', () => {
+
+  it('should retrieve path to local .gitignore', () => {
+    // todo
+  });
+
+  it('should retrieve path to global .gitignore', () => {
+    // todo
+  });
+
+});
+
+describe('.gitignore data insertion', () => {
+
+  it('should append .gitignore data', () => {
+    // todo
+  });
+
+});
+
+describe('.gitignore data retrieval', () => {
+
+  it('should return .gitignore data', () => {
+    // todo
+  });
+
+});
